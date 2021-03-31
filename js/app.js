52e74a311c41f202802062f0d2e82a2a2207753b
Benjamin <benjamin.volenik@vuv.hr>
2021-03-31 10:00:29 +0200

    zad_3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 function IspisTablice()
 {
-	document.getElementById("table").innerHTML += '<tr><td>' + document.getElementById("NazivPredmeta").value + '</td><td>' + document.getElementById("Godina").value + '</td><td>' + document.getElementById("BrojECTS").value + '</td></tr>' ;
+	document.getElementById("table").innerHTML += '<tr><td>' + document.getElementById("NazivPredmeta").value + '</td><td>' + document.getElementById("Godina").value + '</td><td>' + document.getElementById("BrojECTS").value + '</td><td><img src="images/delete.png" onclick="DeleteImage(this)"></td></tr>';
+}
+
+function DeleteImage(r)
+{
+	document.getElementById("table").deleteRow(r.parentNode.parentNode.rowIndex);
 }
