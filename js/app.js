2d317b0a6dfd49b1f6e3ca3de1fa43491db3f7db
Benjamin <benjamin.volenik@vuv.hr>
2021-03-31 10:15:49 +0200

    Zad_3

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 function IspisTablice()
 {
-	document.getElementById("table").innerHTML += '<tr><td>' + document.getElementById("NazivPredmeta").value + '</td><td>' + document.getElementById("Godina").value + '</td><td>' + document.getElementById("BrojECTS").value + '</td><td><img src="images/delete.png" onclick="DeleteImage(this)"></td></tr>';
+	document.getElementById("table").innerHTML += '<tr><td>' + document.getElementById("NazivPredmeta").value + '</td><td>' + document.getElementById("Godina").value + '</td><td>' + document.getElementById("BrojECTS").value + '</td><td><img src="images/delete.png" onclick="DeleteImage(this)"></td><td><input type="checkbox" id="PredmetPolozen" name="PredmetPolozen" onclick="ColorChange(this)"></td></tr>' ;
 }
 
 function DeleteImage(r)
 {
 	document.getElementById("table").deleteRow(r.parentNode.parentNode.rowIndex);
+}
+
+function ColorChange(r)
+{
+	document.getElementById("table").rows[r.parentNode.parentNode.rowIndex].style.backgroundColor = "green";
 }
