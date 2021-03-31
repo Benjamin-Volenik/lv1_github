f631ba7d684a5e26e8095574b5937493b4ea6de6
Benjamin <benjamin.volenik@vuv.hr>
2021-03-31 10:42:54 +0200

    Zad_5

--- js/app.js
+++ js/app.js
@@ -12,3 +12,24 @@ function ColorChange(r)
 {
 	document.getElementById("table").rows[r.parentNode.parentNode.rowIndex].style.backgroundColor = "green";
 }
+
+function PretraziTablicu()
+{
+	  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("Pretraga");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
